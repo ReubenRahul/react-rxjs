492920aca23b187a441b4d235c7895ea5eab8239
Reuben Rahul <reuben.rahul@ithands.biz>
2020-08-17 17:10:05 +0530

    fix(suspense): resolve commitout code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import 'bootstrap/dist/css/bootstrap.css';
-import { Switch, Route, Redirect, BrowserRouter, Link } from 'react-router-dom';
+// import { Switch, Route, Redirect, BrowserRouter, Link } from 'react-router-dom';
 import FormEvent from './FormEvent/FormEvent.component';
 import ofComponent from './OfComponent/of.component';
 import './App.css';
@@ -21,7 +21,7 @@ class App extends React.Component
      <div className="container-fluid">
           <div className="row">
               <ul className="navbar">
-                <li> <a><Link to= "/first">From (Click Handler) Event</Link> </a> </li>
+                {/* <li> <a><Link to= "/first">From (Click Handler) Event</Link> </a> </li>
                 <li> <a><Link to="/of">of Observable</Link> </a> </li>
                 <li> <a> <Link to="/form">From Observable </Link> </a> </li>
                 <li> <a> <Link to="/custom-observable">Custom Observable </Link> </a> </li>
@@ -29,7 +29,7 @@ class App extends React.Component
                 <li> <a className=""> <Link to="/tap-operator">Tap operator </Link> </a></li>
                 <li> <Link to="/take-operator">Take Operator </Link> </li>
                 <li> <Link to="/lists"> List </Link> </li>
-                <li> <Link to="/suspense-user"> Suspense User</Link> </li>
+                <li> <Link to="/suspense-user"> Suspense User</Link> </li> */}
               </ul>
 
           </div>
@@ -40,27 +40,27 @@ class App extends React.Component
    {
 
      return(
-      //  <div> <UserStateLessComponent/></div>
-       <BrowserRouter>
-         <div className="App">
-           <div className="container-fluid">
-           <Switch>
-              <Route path = "/first" component = {FormEvent} />
-              <Route path="/of" component={ofComponent} />
-              <Route path = "/form" component =  {formComponent}/>
-              <Route path = "/to-array" component =  {ToArrayComponent}/>
-              <Route path="/custom-observable" component= {CustomObservableComponent} />
-              <Route path="/map-operator" component= {MapOperatorComponent} />
-              <Route path="/tap-operator" component={TapOperatorComponent} />
-              <Route path="/take-operator" component={TakeComponent} />
-              <Route path="/lists" component={ListComponent} />
-              <Route path="/suspense-user" component={UserComponent} />
-              <Route render = {this.renderHome} />
-           </Switch>
-           </div>
-         </div>
+       <div> <UserStateLessComponent/></div>
+      //  <BrowserRouter>
+      //    <div className="App">
+      //      <div className="container-fluid">
+      //      <Switch>
+      //         <Route path = "/first" component = {FormEvent} />
+      //         <Route path="/of" component={ofComponent} />
+      //         <Route path = "/form" component =  {formComponent}/>
+      //         <Route path = "/to-array" component =  {ToArrayComponent}/>
+      //         <Route path="/custom-observable" component= {CustomObservableComponent} />
+      //         <Route path="/map-operator" component= {MapOperatorComponent} />
+      //         <Route path="/tap-operator" component={TapOperatorComponent} />
+      //         <Route path="/take-operator" component={TakeComponent} />
+      //         <Route path="/lists" component={ListComponent} />
+      //         <Route path="/suspense-user" component={UserComponent} />
+      //         <Route render = {this.renderHome} />
+      //      </Switch>
+      //      </div>
+      //    </div>
 
-       </BrowserRouter>
+      //  </BrowserRouter>
 
      )
    }
